3104cd1cf026536e009e6745e273408e9a9e1cb4
Kul Karki <Kkarki349@rku.ac.in>
2024-10-09 15:23:15 +0530

    update date with body

--- lib/widget/home_page.dart
+++ lib/widget/home_page.dart
@@ -60,6 +60,8 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                     child: ListTile(
                       title: Text(post["title"]),
+                      subtitle: Text(post["body"]),
+                      trailing: const Icon(Icons.person),
                     ),
                   );
                 }));
